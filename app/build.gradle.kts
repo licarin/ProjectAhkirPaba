10fcf3fad83cdf6366d5a4cdd68c1710a25610f5
FamousTeen <125239075+FamousTeen@users.noreply.github.com>
2024-12-26 16:16:15 +0700

    coba push

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,5 +48,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation ("com.google.android.gms:play-services-maps:18.1.0")
+    implementation ("com.google.android.gms:play-services-maps:18.2.0")
 }
