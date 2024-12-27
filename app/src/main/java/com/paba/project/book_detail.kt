95fe70229260d97878a4c96af67c59edd06825e1
FamousTeen <125239075+FamousTeen@users.noreply.github.com>
2024-12-27 20:05:48 +0700

    Book Detail page done

--- app/src/main/java/com/paba/project/book_detail.kt
+++ app/src/main/java/com/paba/project/book_detail.kt
@@ -1,28 +1,34 @@
 package com.paba.project
 
+import android.app.DatePickerDialog
+import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
-import android.widget.Toast
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.gms.common.api.Status
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.firebase.Firebase
-import com.google.firebase.firestore.firestore
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import java.util.Calendar
 
 class book_detail : AppCompatActivity(), OnMapReadyCallback {
-    private var mGoogleMap:GoogleMap? = null
-    var db = Firebase.firestore
-    private val suggestions = mutableListOf<String>()
+    private var mGoogleMap: GoogleMap? = null
+    private val db = Firebase.firestore
+    private var suggestions = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -33,52 +39,7 @@ class book_detail : AppCompatActivity(), OnMapReadyCallback {
             insets
         }
 
-        val dummyAddresses = listOf(
-            address(
-                name = "125 Main St",
-                city = "New York",
-                province = "NY",
-                state = "USA"
-            ),
-            address(
-                name = "456 Elm St",
-                city = "Los Angeles",
-                province = "CA",
-                state = "USA"
-            ),
-            address(
-                name = "789 Oak St",
-                city = "Chicago",
-                province = "IL",
-                state = "USA"
-            ),
-            address(
-                name = "102 Maple Ave",
-                city = "Houston",
-                province = "TX",
-                state = "USA"
-            ),
-            address(
-                name = "202 Pine St",
-                city = "Phoenix",
-                province = "AZ",
-                state = "USA"
-            )
-        )
-
-        // Add each address to the Firestore collection
-        for (address in dummyAddresses) {
-            db.collection("addresses").document(address.name)
-                .set(address)
-                .addOnSuccessListener {
-                    Log.e("successData","Successfully added: $address.name")
-                }
-                .addOnFailureListener { e ->
-                    Log.e("errorData","Error adding document: $e")
-                }
-        }
-
-        var autoCompleteTextView = findViewById<AutoCompleteTextView>(R.id.searchLocation)
+        val autoCompleteTextView = findViewById<AutoCompleteTextView>(R.id.searchLocation)
 
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.mapFragment) as SupportMapFragment
@@ -89,13 +50,76 @@ class book_detail : AppCompatActivity(), OnMapReadyCallback {
 
         autoCompleteTextView.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (!s.isNullOrEmpty()) {
-                    fetchSuggestions(s.toString(), adapter)
+                Log.d("AutoCompleteDebug", "onTextChanged triggered with input: $s")
+                if (s.isNullOrEmpty()) {
+                    suggestions.clear()
+                    adapter.clear()
+                    adapter.notifyDataSetChanged()
+                    Log.d("AutoCompleteDebug", "Query is empty. Suggestions cleared.")
+                    return
                 }
+                fetchSuggestions(s.toString(), adapter)
             }
+
             override fun afterTextChanged(s: Editable?) {}
         })
+
+        val _etSUBirth = findViewById<TextInputEditText>(R.id.taskDateField2)
+
+        _etSUBirth.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            // Tampilkan DatePickerDialog
+            DatePickerDialog(
+                this,
+                { _, selectedYear, selectedMonth, selectedDay ->
+                    // Update EditText dengan tanggal yang dipilih
+                    _etSUBirth.setText("$selectedYear-${selectedMonth + 1}-$selectedDay")
+                },
+                year, month, day
+            ).show()
+        }
+
+        val plusDurBtn = findViewById<Button>(R.id.btn_plus)
+        val minDurBtn = findViewById<Button>(R.id.btn_minus)
+        val taskDuration = findViewById<TextView>(R.id.tv_duration_value1)
+
+        plusDurBtn.setOnClickListener {
+            val currentDuration = taskDuration.text.toString().toIntOrNull() ?: 0
+            taskDuration.setText((currentDuration + 1).toString())
+        }
+        minDurBtn.setOnClickListener {
+            val currentDuration = taskDuration.text.toString().toIntOrNull() ?: 0
+            if (currentDuration > 1) {
+                taskDuration.setText((currentDuration - 1).toString())
+            }
+        }
+
+        var location = autoCompleteTextView.text.toString()
+        var addressDetailText = findViewById<EditText>(R.id.addressDetail).text.toString()
+        var notesText = findViewById<EditText>(R.id.notes).text.toString()
+        var taskDateText = findViewById<TextInputEditText>(R.id.taskDateField2).text.toString()
+        var durationValueText = findViewById<TextView>(R.id.tv_duration_value1).text.toString()
+        var notes2Text = findViewById<EditText>(R.id.editTextTextMultiLine).text.toString()
+
+        val btnBook = findViewById<Button>(R.id.bookNow)
+        btnBook.setOnClickListener {
+//        semesntara book_payment (nanti ganti)
+            val intent = Intent(this, book_payment::class.java).apply {
+                putExtra("SEARCH_LOCATION", location)
+                putExtra("ADDRESS_DETAIL", addressDetailText)
+                putExtra("NOTES", notesText)
+                putExtra("TASK_DATE", taskDateText)
+                putExtra("DURATION_VALUE", durationValueText)
+                putExtra("NOTES2", notes2Text)
+            }
+            startActivity(intent)
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -103,23 +127,45 @@ class book_detail : AppCompatActivity(), OnMapReadyCallback {
     }
 
     private fun fetchSuggestions(query: String, adapter: ArrayAdapter<String>) {
-        db.collection("addresses")
-            .whereGreaterThanOrEqualTo("address", query)
-            .whereLessThanOrEqualTo("name", query + '\uf8ff') // For prefix matching
-            .get()
-            .addOnSuccessListener { documents ->
-                suggestions.clear()
-                for (document in documents) {
-                    val name = document.getString("name")
-                    if (name != null) {
-                        suggestions.add(name)
+        val queryList = listOf("name", "city", "province", "state")
+        suggestions.clear()
+
+        val queriesDone = queryList.size
+        var processedQueries = 0
+
+        for (field in queryList) {
+            db.collection("addresses")
+                .whereGreaterThanOrEqualTo(field, query)
+                .whereLessThanOrEqualTo(field, query + '\uf8ff') // For prefix matching
+                .get()
+                .addOnSuccessListener { documents ->
+                    Log.d("FirestoreDebug", "Number of documents fetched: ${documents.size()}")
+                    for (document in documents) {
+                        val name = document.getString("name")
+                        Log.d("FirestoreDebug", "Name value: $name")
+                        if (name != null && !suggestions.contains(name)) {
+                            suggestions.add(name)
+                        }
+                    }
+                    Log.d("SuggestionsDebug", "Suggestions size: ${suggestions.size}")
+                    processedQueries++
+                    Log.d("SuggestionsDebug", "processedQueries size: ${processedQueries}")
+                    if (processedQueries == queriesDone) {
+                        // Only update the adapter after all queries have been processed
+                        Log.d("SuggestionsDebug", "Suggestions before clearing: ${suggestions.size}")
+                        if(adapter.count != 0) {
+                            adapter.clear()
+                        }
+                        Log.d("SuggestionsDebug", "Suggestions after clearing: ${suggestions.size}")
+                        adapter.addAll(suggestions)
+                        Log.d("SuggestionsDebug", "Suggestions after addAll: ${suggestions.size}")
+                        adapter.notifyDataSetChanged()
+                        Log.d("AutoCompleteDebug3", "Adapter items count after update: ${adapter.count}")
                     }
                 }
-                adapter.notifyDataSetChanged()
-            }
-            .addOnFailureListener { exception ->
-                exception.printStackTrace()
-            }
+                .addOnFailureListener { exception ->
+                    Log.e("FirestoreDebug", "Error fetching data", exception)
+                }
+        }
     }
 }
-
