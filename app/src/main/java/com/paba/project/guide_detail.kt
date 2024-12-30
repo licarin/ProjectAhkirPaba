b23036a0f98318233c404ac801ebf2827390b4cb
Valiant <c14220157@john.petra.ac.id>
2024-12-30 12:05:21 +0700

    guide now

--- app/src/main/java/com/paba/project/guide_detail.kt
+++ app/src/main/java/com/paba/project/guide_detail.kt
@@ -1,12 +1,15 @@
 package com.paba.project
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class guide_detail : AppCompatActivity() {
+    lateinit var _autoComplete: AutoCompleteTextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +19,13 @@ class guide_detail : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        _autoComplete = findViewById<AutoCompleteTextView>(R.id.tv_languange_value)
+        val languange = resources.getStringArray(R.array.languages)
+        val adapter = ArrayAdapter(this, R.layout.dropdown_menu, languange)
+        _autoComplete.setAdapter(adapter)
     }
 }
