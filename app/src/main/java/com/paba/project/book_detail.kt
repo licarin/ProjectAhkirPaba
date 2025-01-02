daf219b6419c96ba935ce3638e2501c70cb1eb4e
FamousTeen <125239075+FamousTeen@users.noreply.github.com>
2025-01-02 13:53:06 +0700

    current location

--- app/src/main/java/com/paba/project/book_detail.kt
+++ app/src/main/java/com/paba/project/book_detail.kt
@@ -1,7 +1,9 @@
 package com.paba.project
 
+
 import android.app.DatePickerDialog
 import android.content.Intent
+import android.location.Location
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -17,25 +19,34 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import java.util.Calendar
+import android.Manifest
+import android.content.pm.PackageManager
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
 
 
 class book_detail : AppCompatActivity(), OnMapReadyCallback {
     private var mGoogleMap: GoogleMap? = null
     private val db = Firebase.firestore
     private var suggestions = mutableListOf<String>()
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,6 +56,33 @@ class book_detail : AppCompatActivity(), OnMapReadyCallback {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            fusedLocationClient.lastLocation.addOnCompleteListener({ task ->
+                if (task.isSuccessful && task.result != null) {
+                    val location: Location = task.result
+                    Log.d(
+                        "CurrentDebug",
+                        "Current Location: Lat: ${location.latitude}, Lng: ${location.longitude}"
+                    )
+                } else {
+                    Log.d("CurrentDebug", "Failed to get location")
+                }
+            })
+        } else {
+            // Request permissions
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                42
+            )
         }
 
         val imageView = findViewById<ImageView>(R.id.guideImage)
